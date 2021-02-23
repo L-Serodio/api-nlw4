26932f891fb7ac45e76c4ccfe31392dc7de9b58f
Lucas_Serodio <53245868+L-Serodio@users.noreply.github.com>
2021-02-23 12:32:17 -0300

    Aula 1 - Iniciando o projeto

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+
+/** HTTP methods
+ * GET => Buscar
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração especifica
+ */
+
+ // http://localhost:3333
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World - NLW#4" });
+});
+
+app.post("/", (request, response) => {
+    // Recebeu os dados para salvar
+    return response.json({ message: "Os dados foram salvos com sucesso!" });
+});
+
+app.listen(3333, () => console.log("Server is running!"));
